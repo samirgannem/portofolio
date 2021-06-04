95b2f545f5e9747c9c90ac2029d8cf1fc2d63bfa
Samir Gannem <sgannem@gmail.com>
2021-06-04 12:15:36 -0400

    reapir icons view and scroll

--- scripts.js
+++ scripts.js
@@ -1,60 +1,32 @@
 const DOMElements = {
-    projectsForm: document.querySelector('.tittle-projects'),
-  };
-const DOMElements1 = {
     skillsForm: document.querySelector('.text-tittle-footer'),
-  };
-const DOMElements2 = {
-    contactForm: document.querySelector('.line-footer'),
   };
 
 window.addEventListener('click', (e) => {
   const eventTarget = e.target;
 
   if (eventTarget.matches('.btn-projects')) {
-    // scrollToContact(DOMElements);
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0;
   }
   
   if (eventTarget.matches('.btn-skills')) {
-    scrollToContact1(DOMElements1);
+    scrollToSkills(DOMElements);
   }  
   
   if (eventTarget.matches('.btn-contact')) {
-    scrollToContact2(DOMElements2);
+    document.body.scrollTop = document.body.scrollHeight; // For Safari
+    document.documentElement.scrollTop = document.documentElement.scrollHeight;
   }
 });
 
-  function scrollToContact(DOMElements) {
-    const { projectsForm} = DOMElements;
-    projectsForm.scrollIntoView({
-      behavior: 'smooth',
-    });
-  }
-
-  function scrollToContact1(DOMElements1) {
-    const { skillsForm} = DOMElements1;
+  function scrollToSkills(DOMElements) {
+    const { skillsForm} = DOMElements;
     skillsForm.scrollIntoView({
       behavior: 'smooth',
     });
   }
 
-  function scrollToContact2(DOMElements2) {
-    const { contactForm} = DOMElements2;
-    contactForm.scrollIntoView({
-      behavior: 'smooth',
-    });
-  }
-
-
-
-  function scrollToTop(DOMElements) {
-    const { heroContainer } = DOMElements;
-    heroContainer.scrollIntoView({
-      behavior: 'smooth',
-    });
-  }
 
       var submitButton = document.getElementById("submit_form");
       var form = document.getElementById("email_form");
